8f25742b4da54baacacd529dc796b39f5ae69555
Chloe <davoine.chloe@telecom-st-etienne.fr>
2023-10-19 15:14:03 +0200

    test

--- sql1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS entities (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255),
-        age INT,
-        weight FLOAT,
-        is_student BOOLEAN
-    );
-    
